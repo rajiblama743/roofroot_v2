bab8e6e8a835658398d3250ce78d155f5079462c
Rajib Lama <rajiblama743@gmail.com>
2025-08-08 00:02:14 +0800

    Fix module resolution with webpack alias configuration

--- roofroot-web/next.config.js
+++ roofroot-web/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add path resolution for @ alias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
